9b48562203a60d86b5f1ede02dfd8ab5807551e1
Yulang <1240894173@qq.com>
2019-05-26 20:25:14 +0800

    round2-3 simple for first time

--- main/main.js
+++ main/main.js
@@ -1,16 +1,7 @@
 module.exports = function main(inputs) {
     // write your code here...
-	var d=inputs.distance;
-	var pt=inputs.parkTime;
-	var price=0;
-	if(d<=2){
-		price=Math.round(6+0.25*pt);
-	}
-	else if(d>2 && d<=8){
-		price=Math.round(6+(d-2)*0.8+0.25*pt);
-	}
-	else{
-		price=Math.round(10.8+(d-8)*1.2+0.25*pt);
-	}
+	
+	var price=Math.round(parkTime * 0.25 +6 +Math.max(0, (distance - 2) * 0.8) +Math.max(0, (distance - 8) * 0.4))
+	
     return price;
 };
