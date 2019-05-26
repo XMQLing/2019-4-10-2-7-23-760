2d2d4ff2866b24db75927c7929dfe0d2dc3bb7cd
Yulang <1240894173@qq.com>
2019-05-26 20:03:32 +0800

    round2-3 0 failtures

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+	var d=inputs.distance;
+	var pt=inputs.parkTime;
+	var price=0;
+	if(d<=2){
+		price=Math.round(6+0.25*pt);
+	}
+	else if(d>2 && d<=8){
+		price=Math.round(6+(d-2)*0.8+0.25*pt);
+	}
+	else{
+		price=Math.round(10.8+(d-8)*1.2+0.25*pt);
+	}
+    return price;
 };
