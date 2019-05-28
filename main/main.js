3b6f92bfb1b87801188c8d36a229561fceb93ecb
Yulang <1240894173@qq.com>
2019-05-28 15:49:55 +0800

    add vname

--- main/main.js
+++ main/main.js
@@ -1,6 +1,7 @@
 module.exports = function main(inputs) {
     // write your code here...
-	
+	parkTime=inputs.parkTime;
+	distance=inputs.distance;
 	var price=Math.round(parkTime * 0.25 +6 +Math.max(0, (distance - 2) * 0.8) +Math.max(0, (distance - 8) * 0.4))
 	
     return price;
